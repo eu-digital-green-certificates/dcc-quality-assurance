0c0461876d3e3b6e92f97e12d06635ef25622359
Milan Kubicek (Detecon) <83577424+mkubicek-dtc@users.noreply.github.com>
2021-06-11 00:02:05 +0200

    Add test data and refactor
    add DE, CH test data
    autopep8 code
    refactor getCertificates

--- tests/test_qrqualitycheck.py
+++ tests/test_qrqualitycheck.py
@@ -34,57 +34,46 @@ from cryptography.hazmat.primitives.hashes import SHA256
 from cryptography.hazmat.primitives.asymmetric import ec, rsa
 from cryptography.utils import int_to_bytes
 
-
 TIMESTAMP_ISO8601_EXTENDED = "%Y-%m-%dT%H:%M:%S.%fZ"
 CONFIG_ERROR = 'CONFIG_ERROR'
+X_RESUME_TOKEN = 'x-resume-token'
+X_KID = 'X-KID'
 ACC_KID_LIST = 'https://dgca-verifier-service-eu-acc.cfapps.eu10.hana.ondemand.com/signercertificateStatus'
-ACC_CERT_LIST= 'https://dgca-verifier-service-eu-acc.cfapps.eu10.hana.ondemand.com/signercertificateUpdate'
+ACC_CERT_LIST = 'https://dgca-verifier-service-eu-acc.cfapps.eu10.hana.ondemand.com/signercertificateUpdate'
+
 
 def pytest_generate_tests(metafunc):
- if "config_env" in metafunc.fixturenames:
-  country_code = metafunc.config.getoption("country_code")
-  file_name = metafunc.config.getoption("file_name")
-  print(country_code, file_name)
-  test_dir = path.dirname(path.dirname(path.abspath(__file__)))
-  test_files = glob(str(Path(test_dir, country_code, "*.png")), recursive=True)
-  metafunc.parametrize("config_env", test_files, indirect=True)
+    if "config_env" in metafunc.fixturenames:
+        country_code = metafunc.config.getoption("country_code")
+        file_name = metafunc.config.getoption("file_name")
+        print(country_code, file_name)
+        test_dir = path.dirname(path.dirname(path.abspath(__file__)))
+        test_files = glob(
+            str(Path(test_dir, country_code, "*", "*.png")), recursive=True)
+        metafunc.parametrize("config_env", test_files, indirect=True)
+
 
 def getKidList():
-    response= requests.get(ACC_KID_LIST)
-    if not response.ok: 
+    response = requests.get(ACC_KID_LIST)
+    if not response.ok:
         fail("KID List not reachable")
     kidlist = dict()
     for x in json.loads(response.text):
-        kidlist[x]=''
+        kidlist[x] = ''
     return kidlist
-      
 
 
 def getCertificates(kidlist):
-    resume_token = 0
-    abort = False
-    while not abort:
-        if resume_token == 0:
-            response= requests.get(ACC_CERT_LIST)
-        else :
-            headers = {"x-resume-token":resume_token}
-            response= requests.get(ACC_CERT_LIST,headers=headers)
-
-        if not response.ok: 
-            fail("Certificate List not reachable")
-        bytes = Sha256.compute_hash(base64.b64decode(response.text))
-
-        kid=base64.b64encode(bytes[0:8]).decode("ascii")
-
-        if kid in kidlist:
-            kidlist[kid] = bytes
-
-        if "x-resume-token" in response.headers:
-            resume_token = response.headers["x-resume-token"]
-        else: 
-            abort= True       
+    r = requests.get(ACC_CERT_LIST)
+    while X_RESUME_TOKEN in r.headers and r.status_code == 200:
+        print(r.headers[X_RESUME_TOKEN])
+        bytes = Sha256.compute_hash(base64.b64decode(r.text))
+        kid = base64.b64encode(bytes[0:8]).decode("ascii")
+        kidlist[r.headers[X_KID]] = bytes
+        r = requests.get(ACC_CERT_LIST, headers={
+                         X_RESUME_TOKEN: r.headers[X_RESUME_TOKEN]})
     return kidlist
-    
+
 
 @filecache(HOUR)
 def downloadCertificates():
@@ -97,84 +86,81 @@ def downloadCertificates():
 def config_env(request):
     # noinspection PyBroadException
     try:
-            config_env = _readobject(request.param)
-            return config_env
+        config_env = _readobject(request.param)
+        return config_env
     except Exception:
         return {CONFIG_ERROR: format_exc()}
 
 
 def _readobject(png):
-    file = open(png,mode='rb')
+    file = open(png, mode='rb')
     # read all lines at once
     all_of_it = file.read()
     # close the file
     file.close()
-    decoded=_get_code_content(all_of_it,png)
+    decoded = _get_code_content(all_of_it, png)
     return decoded
 
-def _get_code_content(b,filepath):
+
+def _get_code_content(b, filepath):
     try:
-     with BytesIO(b) as f:
-        with image_open(f) as image:
-            dec = bar_decode(image)[0]
-     return dec.data.decode()
+        with BytesIO(b) as f:
+            with image_open(f) as image:
+                dec = bar_decode(image)[0]
+        return dec.data.decode()
     except:
         fail(f'QR Code can not be decoded: {filepath}')
 
-def _checkPrefix(decoded): 
+
+def _checkPrefix(decoded):
     return decoded[0:4] == "HC1:"
+
 
 def _object_hook(decoder, value):
     return {k: v.astimezone(timezone.utc).strftime(TIMESTAMP_ISO8601_EXTENDED) if isinstance(v, (date, datetime)) else v
             for k, v in value.items()}
 
+
 def _checkTags(cose):
     firstbyte = cose[0]
     type = "Sign1"
-    if(firstbyte==132):
-      type="List"
+    if(firstbyte == 132):
+        type = "List"
 
-    if(firstbyte==216):
-      type="CWT"
+    if(firstbyte == 216):
+        type = "CWT"
 
-    if(not firstbyte == 210) :
-        fail(f'QR Code not tagged as Sign1 Message. Tagged with {firstbyte} ({type})')
+    if(not firstbyte == 210):
+        fail(
+            f'QR Code not tagged as Sign1 Message. Tagged with {firstbyte} ({type})')
+
 
 def test_issuer_quality(config_env: Dict):
-    
     kidlist = downloadCertificates()
-    
-    _PREFIX=config_env
 
-    if(not _checkPrefix(_PREFIX)) :
-     fail("Prefix not correctly set")
+    _PREFIX = config_env
 
-    _BASE45=_PREFIX[4:]
+    if(not _checkPrefix(_PREFIX)):
+        fail("Prefix not correctly set")
 
-    _COMPRESSION=b45decode(_BASE45)
+    _BASE45 = _PREFIX[4:]
+
+    _COMPRESSION = b45decode(_BASE45)
 
     _COSE = decompress(_COMPRESSION)
 
     _checkTags(_COSE)
 
-    _CBOR= Sign1Message.decode(_COSE)
+    _CBOR = Sign1Message.decode(_COSE)
 
-    alg=_CBOR.phdr[Algorithm]
-    
-    if not alg in ["ES256","PS256"] : 
-        fail(f"Wrong Algorithm used: {alg} Expected: ES256 or PS256")
+    alg = _CBOR.phdr[Algorithm]
+    if not alg.__name__ in ["Ps256", "Es256"]:
+        fail(f"Wrong Algorithm used: {alg.__name__} Expected: Ps256 or Es256")
 
-    alg=_CBOR.uhdr[Algorithm]  
-    
-    if not alg == NONE: 
-        fail("Algorithm must be in Protected header")  
+    if Algorithm in _CBOR.uhdr:
+        fail("Algorithm must be in Protected header")
 
-    print(_CBOR)
+    kid = base64.b64encode(_CBOR.uhdr[KID]).decode("ascii")
 
-    kid = base64.b64encode(_CBOR.phdr[KID]).decode("ascii")
-
-    if not kid in kidlist: 
+    if not kid in kidlist:
         fail("KID exist not on acceptance environment")
-
-    
-    
